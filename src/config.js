d9edaeb2a7911343fa2fbca4ab6d23d914bce178
Roxana Vargas <roxvargas@gmail.com>
2021-10-04 09:09:02 -0500

    Fix config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-exports.port = process.argv[2] || process.env.PORT || 8080;
+exports.port = process.env.PORT || 8080;
 exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
